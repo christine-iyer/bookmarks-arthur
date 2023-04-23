48ddae4aad7ee99b48db068ac476e2c93d800c64
chrisIyer <hellohotbox420@gmail.com>
2023-04-23 13:38:03 -0400

    tidying

--- models/bookmark.js
+++ models/bookmark.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose')
 const bookmarkSchema = new Schema({
     title: { type: String, required: true },
     category: {type: String},
-    url: { type: String },
+    images: [{ type: mongoose.Schema.Types.ObjectId, ref: 'imageUpload' }],
     body: { type: String }
 }, {
     timestamps: true
